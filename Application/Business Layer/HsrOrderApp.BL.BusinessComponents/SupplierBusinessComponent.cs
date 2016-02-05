92524bebdad1b3ed9d97e42ba01f994c83bc7b2a
Tanja <tanja.neuenschwander@gmail.com>
2016-02-05 09:30:46 +0100

    updated Adapter

---
+++ Application/Business Layer/HsrOrderApp.BL.BusinessComponents/SupplierBusinessComponent.cs
@@ -0,0 +1,106 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using HsrOrderApp.BL.DomainModel;
+using HsrOrderApp.DAL.Data.Repositories;
+using HsrOrderApp.SharedLibraries.SharedEnums;
+
+#endregion
+
+namespace HsrOrderApp.BL.BusinessComponents
+{
+    public class SupplierBusinessComponent
+    {
+        private ISupplierRepository rep;
+
+        public SupplierBusinessComponent()
+        {
+        }
+
+        public SupplierBusinessComponent(ISupplierRepository unitOfWork)
+        {
+            this.rep = unitOfWork;
+        }
+
+        public ISupplierRepository Repository
+        {
+            get { return this.rep; }
+            set { this.rep = value; }
+        }
+
+        public Supplier GetSupplierById(int supplierId)
+        {
+            Supplier supplier = rep.GetById(supplierId);
+            return supplier;
+        }
+
+        public IQueryable<Supplier> GetSuppliersByCriteria(SupplierSearchType searchType, int productId, string supplierName)
+        {
+            IQueryable<Supplier> suppliers = new List<Supplier>().AsQueryable();
+
+            switch (searchType)
+            {
+                case SupplierSearchType.None:
+                    suppliers = rep.GetAll();
+                    break;
+                case SupplierSearchType.ByProduct:
+                    suppliers = rep.GetAll().Where(su => su.Conditions.Any(c => c.Product.ProductId == productId));
+                    break;
+                case SupplierSearchType.ByName:
+                    suppliers = rep.GetAll().Where(cu => cu.Name == supplierName);
+                    break;
+            }
+            return suppliers;
+        }
+
+        public int StoreSupplier(Supplier supplier, IEnumerable<ChangeItem> changeItems)
+        {
+            int supplierId = default(int);
+            using (TransactionScope transaction = new TransactionScope())
+            {
+                supplierId = rep.SaveSupplier(supplier);
+                foreach (ChangeItem change in changeItems)
+                {
+                    if (change.Object is Address)
+                    {
+                        Address address = (Address) change.Object;
+                        switch (change.ChangeType)
+                        {
+                            case ChangeType.ChildInsert:
+                            case ChangeType.ChildUpate:
+                                rep.SaveAddress(address, supplier);
+                                break;
+                            case ChangeType.ChildDelete:
+                                rep.DeleteAddress(address.AddressId);
+                                break;
+                        }
+                    }
+                    else if (change.Object is SupplierCondition)
+                    {
+                        SupplierCondition condition = (SupplierCondition) change.Object;
+                        switch (change.ChangeType)
+                        {
+                            case ChangeType.ChildInsert:
+                            case ChangeType.ChildUpate:
+                                rep.SaveCondition(condition, supplier);
+                                break;
+                            case ChangeType.ChildDelete:
+                                rep.DeleteCondition(condition.ConditionId);
+                                break;
+                        }
+                    }
+                }
+                transaction.Complete();
+            }
+
+            return supplierId;
+        }
+
+        public void DeleteSupplier(int supplierId)
+        {
+            rep.DeleteSupplier(supplierId);
+        }
+    }
+}
---
+++ Application/Business Layer/HsrOrderApp.BL.DTOAdapters/SupplierAdapter.cs
@@ -0,0 +1,145 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.Linq;
+using HsrOrderApp.BL.BusinessComponents;
+using HsrOrderApp.BL.DomainModel;
+using HsrOrderApp.BL.DomainModel.HelperObjects;
+using HsrOrderApp.BL.DTOAdapters.Helper;
+using HsrOrderApp.SharedLibraries.DTO;
+using HsrOrderApp.SharedLibraries.DTO.Base;
+
+#endregion
+
+namespace HsrOrderApp.BL.DtoAdapters
+{
+    public class SupplierAdapter
+    {
+        #region SupplierToDTO
+
+        public static IList<SupplierListDTO> SuppliersToDtos(IQueryable<Supplier> suppliers)
+        {
+            IQueryable<SupplierListDTO> supplierDtos = from c in suppliers
+                                                       select new SupplierListDTO()
+                                                                  {
+                                                                      Id = c.SupplierId,
+                                                                      Name = c.Name,
+                                                                      AccountNumber = c.AccountNumber,
+                                                                      ActiveFlag = c.ActiveFlag,
+                                                                      CreditRating = c.CreditRating,
+                                                                      PreferredSupplierFlag = c.PreferredSupplierFlag
+                                                                  };
+            return supplierDtos.ToList();
+        }
+
+        public static SupplierDTO SupplierToDto(Supplier c)
+        {
+            SupplierDTO dto = new SupplierDTO()
+                                  {
+                                      Id = c.SupplierId,
+                                      Name = c.Name,
+                                      AccountNumber = c.AccountNumber,
+                                      ActiveFlag = c.ActiveFlag,
+                                      CreditRating = c.CreditRating,
+                                      PreferredSupplierFlag = c.PreferredSupplierFlag,
+                                      PurchasingWebServiceURL = c.PurchasingWebServiceURL,
+                                      Version = c.Version,
+                                      Addresses = AddressAdapter.AddressToDtos(c.Addresses),
+                                      Conditions = ConditionsToDtos(c.Conditions)
+                                  };
+
+            return dto;
+        }
+
+        public static IList<SupplierConditionDTO> ConditionsToDtos(IQueryable<SupplierCondition> conditions)
+        {
+            IQueryable<SupplierConditionDTO> supplierConditionsDTOs = from c in conditions
+                                                                      select ConditionToDto(c);
+            return supplierConditionsDTOs.ToList();
+        }
+
+        public static SupplierConditionDTO ConditionToDto(SupplierCondition c)
+        {
+            SupplierConditionDTO dto = new SupplierConditionDTO()
+                                           {
+                                               Id = c.ConditionId,
+                                               AverageLeadTime = c.AverageLeadTimeInDays,
+                                               LastReceiptCost = c.LastReceiptCost,
+                                               LastReceiptDate = c.LastReceiptDate,
+                                               MaxOrderQuantity = c.MaxOrderQuantity,
+                                               MinOrderQuantity = c.MinOrderQuantity,
+                                               StandardPrice = c.StandardPrice,
+                                               ProductId = c.Product.ProductId,
+                                               ProductName = c.Product.Name,
+                                               Version = c.Version
+                                           };
+            return dto;
+        }
+
+        #endregion
+
+        #region DTOToSupplier
+
+        public static Supplier DtoToSupplier(SupplierDTO dto)
+        {
+            Supplier supplier = new Supplier()
+                                    {
+                                        SupplierId = dto.Id,
+                                        Name = dto.Name,
+                                        AccountNumber = dto.AccountNumber,
+                                        ActiveFlag = dto.ActiveFlag,
+                                        CreditRating = dto.CreditRating,
+                                        PreferredSupplierFlag = dto.PreferredSupplierFlag,
+                                        PurchasingWebServiceURL = dto.PurchasingWebServiceURL,
+                                        Version = dto.Version
+                                    };
+            ValidationHelper.Validate(supplier);
+            return supplier;
+        }
+
+        public static IEnumerable<ChangeItem> GetChangeItems(SupplierDTO dto, Supplier supplier)
+        {
+            IEnumerable<ChangeItem> changeItems = from c in dto.Changes
+                                                  select
+                                                      new ChangeItem(c.ChangeType,
+                                                                     DtoChildToBusinessChild(c.Object,
+                                                                                             supplier));
+            return changeItems;
+        }
+
+        private static DomainObject DtoChildToBusinessChild(DTOVersionObject dto, Supplier supplier)
+        {
+            if (dto is AddressDTO)
+            {
+                return AddressAdapter.DtoToAddress((AddressDTO) dto);
+            }
+            else if (dto is SupplierConditionDTO)
+            {
+                return DtoToCondition((SupplierConditionDTO) dto, supplier);
+            }
+            return null;
+        }
+
+        public static SupplierCondition DtoToCondition(SupplierConditionDTO dto, Supplier supplier)
+        {
+            SupplierCondition condition = new SupplierCondition
+                                              {
+                                                  ConditionId = dto.Id,
+                                                  AverageLeadTimeInDays = dto.AverageLeadTime,
+                                                  LastReceiptCost = dto.LastReceiptCost,
+                                                  LastReceiptDate = dto.LastReceiptDate,
+                                                  MaxOrderQuantity = dto.MaxOrderQuantity,
+                                                  MinOrderQuantity = dto.MinOrderQuantity,
+                                                  StandardPrice = dto.StandardPrice,
+                                                  Supplier = supplier,
+                                                  Version = dto.Version,
+                                                  Product = new Product {ProductId = dto.ProductId}
+                                              };
+
+            ValidationHelper.Validate(condition);
+            return condition;
+        }
+
+        #endregion
+    }
+}
